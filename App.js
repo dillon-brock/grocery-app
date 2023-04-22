6ceba269c354e39a5dd7ffd89518720cc82d1a77
Dillon Brock <mdillon1297@gmail.com>
2023-04-22 14:01:14 -0700

    moved auth entry into its own component

--- App.js
+++ App.js
@@ -1,64 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { useState } from 'react';
+// import { Pressable, StyleSheet, Text, View } from 'react-native';
+import AuthEntry from './components/AuthEntry';
 
 export default function App() {
 
-  const [logInPressed, setLogInPressed] = useState(false);
-  const [signUpPressed, setSignUpPressed] = useState(false);
-
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Grocery App</Text>
-      <View style={styles.buttonContainer}>
-        <Pressable 
-          onPressIn={() => setLogInPressed(true)}
-          onPressOut={() => setLogInPressed(false)}
-          style={{backgroundColor: logInPressed ? "white" : "black", ...styles.button}}>
-          <Text style={{color: logInPressed ? "black" : "white", ...styles.buttonText}}>Log In</Text>
-        </Pressable>
-        <Pressable 
-          onPressIn={() => setSignUpPressed(true)}
-          onPressOut={() => setSignUpPressed(false)}
-          style={{backgroundColor: signUpPressed ? "white" : "black", ...styles.button}}>
-          <Text style={{color: signUpPressed ? "black" : "white", ...styles.buttonText}}>Sign Up</Text>
-        </Pressable>
-      </View>
-      <StatusBar style="auto" />
-    </View>
+    <AuthEntry />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    gap: 60
-  },
-  title: {
-    fontSize: 50,
-    fontFamily: 'Avenir-Oblique'
-  },
-  buttonContainer: {
-    paddingTop: 80,
-    gap: 40
-  },
-  button: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 12,
-    paddingHorizontal: 32,
-    borderRadius: 5,
-    borderWidth: 4,
-    borderColor: 'black',
-    elevation: 3,
-  },
-  buttonText: {
-    fontSize: 16,
-    lineHeight: 21,
-    fontWeight: 'bold',
-    letterSpacing: 0.25,
-  },
-});
