f06625f4a99c5b82fede44c0eeaeef7cf45733a5
Dillon Brock <mdillon1297@gmail.com>
2023-04-22 13:42:32 -0700

    auth buttons change appearance when pressed, still no functionality

--- App.js
+++ App.js
@@ -1,17 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [logInPressed, setLogInPressed] = useState(false);
+  const [signUpPressed, setSignUpPressed] = useState(false);
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Grocery App</Text>
       <View style={styles.buttonContainer}>
-        <Pressable style={styles.button}>
-          <Text style={styles.buttonText}>Log In</Text>
+        <Pressable 
+          onPressIn={() => setLogInPressed(true)}
+          onPressOut={() => setLogInPressed(false)}
+          style={{backgroundColor: logInPressed ? "white" : "black", ...styles.button}}>
+          <Text style={{color: logInPressed ? "black" : "white", ...styles.buttonText}}>Log In</Text>
         </Pressable>
-        <Pressable style={styles.button}>
-          <Text style={styles.buttonText}>Sign Up</Text>
+        <Pressable 
+          onPressIn={() => setSignUpPressed(true)}
+          onPressOut={() => setSignUpPressed(false)}
+          style={{backgroundColor: signUpPressed ? "white" : "black", ...styles.button}}>
+          <Text style={{color: signUpPressed ? "black" : "white", ...styles.buttonText}}>Sign Up</Text>
         </Pressable>
       </View>
       <StatusBar style="auto" />
@@ -40,15 +50,15 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     paddingVertical: 12,
     paddingHorizontal: 32,
-    borderRadius: 4,
+    borderRadius: 5,
+    borderWidth: 4,
+    borderColor: 'black',
     elevation: 3,
-    backgroundColor: 'black',
   },
   buttonText: {
     fontSize: 16,
     lineHeight: 21,
     fontWeight: 'bold',
     letterSpacing: 0.25,
-    color: 'white',
   },
 });
