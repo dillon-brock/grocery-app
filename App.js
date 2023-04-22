a77fcd7e7ea3a0b9c609efc9eae95ac8f41e15f2
Dillon Brock <mdillon1297@gmail.com>
2023-04-22 14:04:17 -0700

    removed commented out code

--- App.js
+++ App.js
@@ -1,6 +1,3 @@
-// import { StatusBar } from 'expo-status-bar';
-// import { useState } from 'react';
-// import { Pressable, StyleSheet, Text, View } from 'react-native';
 import AuthEntry from './components/AuthEntry';
 
 export default function App() {
