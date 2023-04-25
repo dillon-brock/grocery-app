bc239f6892fc02c636431fe60c636d82e5c5c76e
Dillon Brock <mdillon1297@gmail.com>
2023-04-25 15:34:22 -0700

    moved stylesheet for PrimaryButton component into its own file in styles/

--- components/PrimaryButton.tsx
+++ components/PrimaryButton.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { useState } from "react";
-import { Pressable, StyleSheet, Text } from "react-native";
+import { Pressable, Text } from "react-native";
 import { PrimaryButtonProps } from "../types/props";
+import styles from '../styles/primaryButton';
 
 export default function PrimaryButton({ text, handlePress }: PrimaryButtonProps) {
 
@@ -23,22 +24,3 @@ export default function PrimaryButton({ text, handlePress }: PrimaryButtonProps)
       </Pressable>
   )
 }
-
-const styles = StyleSheet.create({
-  button: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 12,
-    paddingHorizontal: 32,
-    borderRadius: 5,
-    borderWidth: 4,
-    borderColor: 'black',
-    elevation: 3,
-  },
-  buttonText: {
-    fontSize: 16,
-    lineHeight: 21,
-    fontWeight: 'bold',
-    letterSpacing: 0.25,
-  },
-})
---
+++ styles/primaryButton.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 5,
+    borderWidth: 4,
+    borderColor: 'black',
+    elevation: 3,
+  },
+  buttonText: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+  }
+})
