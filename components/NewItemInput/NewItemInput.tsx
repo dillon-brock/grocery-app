92f40c5a6c80fa2861d9c7d1a69b311d5b4c2162
Dillon Brock <mdillon1297@gmail.com>
2023-05-02 22:14:26 -0700

    created new item input component (no styling yet)

--- components/NewItemInput/NewItemInput.tsx
+++ components/NewItemInput/NewItemInput.tsx
@@ -1,15 +1,35 @@
-import React, { useState } from "react";
+import React, { Dispatch, SetStateAction, useState } from "react";
 import { View } from "react-native";
 import Input from "../Input/Input";
+import IconButton from "../IconButton/IconButton";
+import { addItemToList } from "../../services/list-items/list-items";
+import { ListWithItems } from "../../types/types";
 
-export default function NewItemInput() {
+type Props = {
+  listId: string;
+  setList: Dispatch<SetStateAction<ListWithItems>>
+}
 
-  const [item, setItem] = useState('');
+export default function NewItemInput({ listId, setList }: Props) {
+
+  const [item, setItem] = useState<string>('');
+  const [quantity, setQuantity] = useState<number>(1);
+
+  const handleAddItem = async (): Promise<void> => {
+    const addItemRes = await addItemToList({ listId, item, quantity });
+    if (addItemRes.success) {
+      setList((prev: ListWithItems) => ({ 
+        ...prev, 
+        items: [...prev.items, addItemRes.item] 
+      }));
+    }
+  }
 
   return (
     <View>
       <Input type="text" value={item} onChange={(e) => setItem(e.nativeEvent.text)} />
-      <Input type="number" />
+      <Input type="number" value={String(quantity)} onChange={(e) => setQuantity(Number(e.nativeEvent.text))}/>
+      <IconButton name="add-circle" handlePress={handleAddItem} />
     </View>
   )
 }
