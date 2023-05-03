bfa3a279a4d7c5d8c4c8ac9c40e259967b6df379
Dillon Brock <mdillon1297@gmail.com>
2023-05-03 12:49:56 -0700

    created list item component, set up to delete and update item

---
+++ components/ListItem/ListItem.tsx
@@ -0,0 +1,43 @@
+import React, { Dispatch, SetStateAction } from "react";
+import { Pressable, Text } from "react-native";
+import { deleteItem, updateItem } from "../../services/list-items/list-items";
+import { type ListItem, ListWithItems } from "../../types/types";
+
+type Props = {
+  id: string;
+  item: string;
+  quantity: number | null;
+  bought: boolean;
+  setList: Dispatch<SetStateAction<ListWithItems>>;
+}
+
+export default function ListItem({ id, item, quantity, bought, setList }: Props) {
+
+  const handleDeleteItem = async (): Promise<void> => {
+    await deleteItem(id);
+    setList((prev: ListWithItems) => ({
+      ...prev,
+      items: prev.items.filter((listItem: ListItem) => listItem.id != id)
+    }));
+  }
+
+  const handleToggleBought = async (): Promise<void> => {
+    await updateItem(id, { bought: !bought });
+    setList((prev: ListWithItems): ListWithItems => {
+      const itemToBeUpdated = prev.items.find((i: ListItem) => i.id == id);
+      if (!itemToBeUpdated) return prev;
+      return {
+      ...prev,
+      items: [
+        ...prev.items.filter((i: ListItem) => i.id != id),
+        { ...itemToBeUpdated, bought: !bought }
+      ]
+    }})
+  }
+
+  return (
+    <Pressable onLongPress={handleDeleteItem} onPress={handleToggleBought}>
+      <Text>{`${quantity}   ${item}: ${bought}`}</Text>
+    </Pressable>
+  )
+}
