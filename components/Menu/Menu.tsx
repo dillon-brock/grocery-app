4bd4db11d190a31bef81f6d3b591f30d49647a79
Dillon Brock <mdillon1297@gmail.com>
2023-05-02 21:19:53 -0700

    improvements to hamburger menu (close button and fullscreen)

--- components/Menu/Menu.tsx
+++ components/Menu/Menu.tsx
@@ -1,6 +1,6 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { View } from "react-native";
+import { Pressable, View } from "react-native";
 import MenuItem from '../MenuItem/MenuItem';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { HomeStackParamList } from '../../types/types';
@@ -8,11 +8,12 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useUserContext } from '../../context/UserContext';
 import styles from './styles';
 import { useMenuContext } from '../../context/MenuContext';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function Menu() {
 
   const { setUser } = useUserContext();
-  const { setMenuOpen } = useMenuContext();
+  const { menuOpen, setMenuOpen } = useMenuContext();
 
   const navigation = useNavigation<NativeStackNavigationProp<HomeStackParamList>>();
 
@@ -33,21 +34,28 @@ export default function Menu() {
   }
 
   return (
-    <View style={styles.container}>
-      <View style={styles.menu}>
-        <MenuItem 
-          text="Home" 
-          handlePress={handleGoHome}
-        />
-        <MenuItem
-          text="My Lists"
-          handlePress={handleGoToLists}
-        />
-        <MenuItem
-          text = "Log out"
-          handlePress={handleLogOut}
-        />
+    <>
+    {menuOpen &&
+      <View style={styles.container}>
+        <View style={styles.menu}>
+          <Pressable style={styles.closeButton} onPressOut={() => setMenuOpen(false)}>
+            <Ionicons name="close-outline" size={30} />
+          </Pressable>
+          <MenuItem 
+            text="Home" 
+            handlePress={handleGoHome}
+          />
+          <MenuItem
+            text="My Lists"
+            handlePress={handleGoToLists}
+          />
+          <MenuItem
+            text = "Log out"
+            handlePress={handleLogOut}
+          />
+        </View>
       </View>
-    </View>
-  )
+    }
+    </>
+    )
 }
---
+++ components/NewItemInput/NewItemInput.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+import Input from "../Input/Input";
+
+export default function NewItemInput() {
+  return (
+    <View>
+      <Input type="text" />
+    </View>
+  )
+}
