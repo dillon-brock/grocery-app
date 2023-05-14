9496d755b088421e9140bce4977670084b670366
Dillon Brock <mdillon1297@gmail.com>
2023-05-13 22:56:25 -0700

    ingredients displaying conditionally based on whether recipe is locked

--- screens/RecipeDetailScreen/RecipeDetailScreen.tsx
+++ screens/RecipeDetailScreen/RecipeDetailScreen.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { ScrollView, Text, View } from "react-native";
 import Header from "../../components/Header/Header";
 import { RouteProp, useRoute } from "@react-navigation/native";
@@ -7,11 +7,14 @@ import { useRecipe } from "../../hooks/useRecipe";
 import NewItemInput from "../../components/NewItemInput/NewItemInput";
 import { addIngredient } from "../../services/ingredients/ingredients";
 import Ingredient from "../../components/Ingredient/Ingredient";
+import LockButton from "../../components/LockButton/LockButton";
+import EditableListItem from "../../components/EditableListItem/EditableListItem";
 
 export default function RecipeDetailScreen() {
 
   const { recipeId } = useRoute<RouteProp<RecipeStackParamList, 'RecipeDetail'>>().params;
   const { recipe, setRecipe, loading } = useRecipe(recipeId);
+  const [locked, setLocked] = useState<boolean>(true);
 
   const handleAddIngredient = async (name: string, amount: string): Promise<void> => {
     const newIngredientRes = await addIngredient(recipeId, { name, amount });
@@ -29,6 +32,7 @@ export default function RecipeDetailScreen() {
   return (
     <View>
       <Header showBackButton showMenuButton />
+      <LockButton locked={locked} setLocked={setLocked} />
       <ScrollView keyboardShouldPersistTaps='handled'>
         {loading && <Text>Loading...</Text>}
         {!loading &&
@@ -36,11 +40,21 @@ export default function RecipeDetailScreen() {
             <Text>{recipe.name}</Text>
             <View>
               <Text>Ingredients</Text>
-              {recipe.ingredients.map(ingredient => (
-                <Ingredient
-                  name={ingredient.name}
-                  amount={ingredient.amount} />
-              ))}
+              {recipe.ingredients.map(ingredient => {
+                if (locked) {
+                  return <Ingredient
+                    name={ingredient.name}
+                    amount={ingredient.amount} />
+                }
+                else return (
+                  <EditableListItem 
+                    id={ingredient.id} 
+                    quantity={ingredient.amount}
+                    item={ingredient.name}
+                    handleUpdateItem={async () => {null}}
+                    handleUpdateQuantity={async () => {null}} />
+                )
+              })}
               <NewItemInput handleAdd={handleAddIngredient} />
             </View>
             <View>
