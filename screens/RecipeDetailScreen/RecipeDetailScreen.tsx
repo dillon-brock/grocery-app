71fa352cef4e5e82ab7f54108baa752e9c61d8a5
Dillon Brock <mdillon1297@gmail.com>
2023-05-19 19:16:21 -0700

    added navigation to share screen on recipe detail screen

--- screens/RecipeDetailScreen/RecipeDetailScreen.tsx
+++ screens/RecipeDetailScreen/RecipeDetailScreen.tsx
@@ -1,25 +1,37 @@
 import React, { useState } from "react";
 import { ScrollView, Text, View } from "react-native";
 import Header from "../../components/molecules/Header/Header";
-import { RouteProp, useRoute } from "@react-navigation/native";
-import { RecipeStackParamList } from "../../types/types";
+import { RouteProp, useNavigation, useRoute } from "@react-navigation/native";
+import { HomeStackParamList, RecipeStackParamList } from "../../types/types";
 import { useRecipe } from "../../hooks/useRecipe";
 import LockButton from "../../components/atoms/buttons/LockButton/LockButton";
 import IngredientList from "../../components/organisms/IngredientList/IngredientList";
 import StepList from "../../components/organisms/StepList/StepList";
 import styles from './styles';
 import ScreenTitle from "../../components/atoms/ScreenTitle/Title";
+import ShareButton from "../../components/atoms/buttons/ShareButton/ShareButton";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 
 export default function RecipeDetailScreen() {
 
   const { recipeId } = useRoute<RouteProp<RecipeStackParamList, 'RecipeDetail'>>().params;
   const { recipe, ingredients, setIngredients, steps, setSteps, loading } = useRecipe(recipeId);
   const [locked, setLocked] = useState<boolean>(true);
+  const navigation = useNavigation<NativeStackNavigationProp<HomeStackParamList>>();
+
+  const goToShareScreen = () => {
+    navigation.navigate('Share', {
+      id: recipeId,
+      name: recipe.name,
+      type: 'recipe'
+    })
+  }
 
   return (
     <View style={styles.screen}>
       <Header showBackButton showMenuButton />
       <LockButton locked={locked} setLocked={setLocked} />
+      <ShareButton goToShareScreen={goToShareScreen} />
       <ScrollView keyboardShouldPersistTaps='handled'>
         {loading && <Text>Loading...</Text>}
         {!loading &&
